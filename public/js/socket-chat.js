fcaa8077f75f3f74706b7f2093f54203fb3d747b
ebros <ebros121@gmail.com>
2019-08-20 10:24:23 -0500

    Chat Finalizado v1

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -3,19 +3,25 @@ var socket = io();
 var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
-    window.location = 'index.html'
-    throw new Error('El nombre y sala son necesario');
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
+
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log('Usuario Conectados', resp);
+        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp)
     });
+
 });
 
 // escuchar
@@ -27,33 +33,30 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje)
 });
 
-//Entrar o salir de chat
-
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-
-
+    console.log(personas);
+    renderizarUsuarios(personas, false)
+    scrollBottom();
 });
 
-
-
-
-//Mensajes privados
-
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje Privado', mensaje);
-})
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
